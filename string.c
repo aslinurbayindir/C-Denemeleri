31489019addf57f0e0d47579a859516f03e674e5
aslinurb <aslinurbayindir@gmail.com>
2019-03-07 14:28:17 +0300

    Create string.c

---
+++ string.c
@@ -0,0 +1,8 @@
+include <stdio.h>
+int main (){
+
+    char isim[20];
+    scanf("%s",isim); //stringler %s ile çalışır ve & işareti konmaz çünkü diziler zaten pointer
+    printf("tanistigimiza memnun oldum %s",isim);
+    
+}
